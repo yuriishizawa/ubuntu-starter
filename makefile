9b365918d4b6da3fcf4544c4726932b04609d142
Yuri Ishizawa <yuri.ishizawa@magazineluiza.com.br>
2022-02-11 00:33:43 -0300

    Adding vim config

--- makefile
+++ makefile
@@ -1,2 +1,21 @@
 auto_commit:
 	git add . && git commit -m "Update" && git push
+
+install:
+	apt-get install python3-pip -y && pip install pynvim && \
+		curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+install_nvim:
+	nvm install --lts && apt-get install vim nvim -y
+
+convert_vim_config:
+	cp vimrc .vimrc
+
+copy_nvim:
+	cp .vimrc .config/nvim/init.vim
+
+install_vim_plug:
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
+			sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plu
+      				 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
--- script.sh
+++ script.sh
@@ -2,15 +2,13 @@
 
 apt update && apt upgrade -y
 
-apt install arduino bleachbit qgis curl wget htop python3-dev python3-pip fritzing texstudio texlive gimp texlive-full obs-studio kdenlive texlive-lang-portuguese texlive-latex-extra vlc r-base telegram-desktop git -y
+apt install curl wget htop python3-dev python3-pip texstudio texlive gimp texlive-full obs-studio texlive-lang-portuguese texlive-latex-extra vlc r-base -y
 snap install spotify
-snap install discord
 snap install slack --classic
 snap install sublime-text --classic
 snap install code --classic
 
 wget -c https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.4.1106-amd64.deb
-wget -c https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 
 anaconda_version=$(curl -s https://repo.anaconda.com/archive/ | grep -e 'Anaconda3-2021.[0-2][0-9]-Linux-x86' | tail -n 1 | cut -d "\"" -f 2)
 
