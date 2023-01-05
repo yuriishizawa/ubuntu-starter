ad30a0d097a0bb0fa177986bdb99122d3a0e44a3
Yuri Batista Ishizawa <yuriishizawa@gmail.com>
2023-01-05 09:37:33 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,9 +8,10 @@ snap install slack --classic
 snap install sublime-text --classic
 snap install code --classic
 
-wget -c https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.4.1106-amd64.deb
+rstudio_version=$(curl -s https://posit.co/download/rstudio-desktop/ | grep -P 'rstudio-.*\.deb' -o | sort | tail -n 1)
+wget -c https://download1.rstudio.org/desktop/bionic/amd64/$rstudio_version
 
-anaconda_version=$(curl -s https://repo.anaconda.com/archive/ | grep -e 'Anaconda3-20[2-9][0-9].[0-2][0-9]-Linux-x86' | sort | tail -n 1 | cut -d "\"" -f 2)
+anaconda_version=$(curl -s https://repo.anaconda.com/archive/ | grep -e 'Anaconda3-20.*-Linux-x86_64\.sh' | sort | tail -n 1| cut -d "\"" -f 2)
 
 wget -c -O Anaconda.sh https://repo.anaconda.com/archive/$anaconda_version
 chmod a+x ./Anaconda.sh
