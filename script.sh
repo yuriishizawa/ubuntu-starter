b392e8192ea22c7c4b0d9b6cdf73322d044dfd05
Yuri Ishizawa <yuriishizawa@gmail.com>
2024-06-30 23:38:35 -0300

    chore: update script.sh with additional package installations and configurations

--- script.sh
+++ script.sh
@@ -2,21 +2,40 @@
 
 apt update && apt upgrade -y
 
-apt install curl wget htop python3-dev python3-pip texstudio texlive gimp texlive-full obs-studio texlive-lang-portuguese texlive-latex-extra vlc r-base -y
+# aplicativos básicos
+apt install curl git wget lm-sensors htop python3-dev python3-pip python3-tk tk-dev libsqlite3-dev texstudio texlive gimp texlive-full obs-studio texlive-lang-portuguese texlive-latex-extra vlc r-base -y
 snap install spotify
-snap install slack --classic
 snap install sublime-text --classic
 snap install code --classic
 
-rstudio_version=$(curl -s https://posit.co/download/rstudio-desktop/ | grep -P 'rstudio-.*\.deb' -o | sort | tail -n 1)
-wget -c https://download1.rstudio.org/desktop/bionic/amd64/$rstudio_version
+# rstudio_version=$(curl -s https://posit.co/download/rstudio-desktop/ | grep -P 'rstudio-.*\.deb' -o | sort | tail -n 1)
+# wget -c https://download1.rstudio.org/desktop/bionic/amd64/$rstudio_version
 
-anaconda_version=$(curl -s https://repo.anaconda.com/archive/ | grep -e 'Anaconda3-20.*-Linux-x86_64\.sh' | sort | tail -n 1| cut -d "\"" -f 2)
+# anaconda_version=$(curl -s https://repo.anaconda.com/archive/ | grep -e 'Anaconda3-20.*-Linux-x86_64\.sh' | sort | tail -n 1| cut -d "\"" -f 2)
 
-wget -c -O Anaconda.sh https://repo.anaconda.com/archive/$anaconda_version
-chmod a+x ./Anaconda.sh
-./Anaconda.sh
+# wget -c -O Anaconda.sh https://repo.anaconda.com/archive/$anaconda_version
+# chmod a+x ./Anaconda.sh
+# ./Anaconda.sh
 
-dpkg -i *.deb
+# dpkg -i *.deb
 
-apt install ubuntu-restricted-extras
+# extras
+apt install ubuntu-restricted-extras -y
+
+# poetry
+curl -sSL https://install.python-poetry.org | python3 -
+
+# docker
+apt install docker.io -y
+
+# docker-compose
+apt install docker-compose -y
+
+# pyenv
+curl https://pyenv.run | bash
+
+# zsh
+apt install zsh -y
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # https://dev.to/henriquemsimoes/instalando-e-configurando-zsh-e-oh-my-zsh-4bem
